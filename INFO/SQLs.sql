3cc06bc04848ad790571393faf0670e4f70149a8
XIIIv2 <kirill.popov@icloud.com>
2025-06-14 01:55:07 +0300

    Update SQLs.sql

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `spring_web_mvc_contact`
 /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE TABLE spring_web_mvc_contact.contacts (
+CREATE TABLE IF NOT EXISTS spring_web_mvc_contact.contacts (
 	id BIGINT UNSIGNED auto_increment NOT NULL,
 	first_name varchar(128) NOT NULL,
 	last_name varchar(128) NOT NULL,
